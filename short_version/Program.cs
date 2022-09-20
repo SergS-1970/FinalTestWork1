71093282b6f7c3eefd439eec3c8d0d6b70f2bd6a
Serg_Sytnikov <tlingaal1996@gmail.com>
2022-09-20 22:40:17 +0300

    add short_version

---
+++ short_version/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+лучше обойтись исключительно массивами. */
+
+Console.WriteLine("Введите размер исходного массива: ");
+int sizeA = Convert.ToInt32(Console.ReadLine());
+
+string[] sourceArrayA = new string[sizeA];
+
+string[] finalArrayF = new string[sizeA];
+int shortElementCounter = 0;
+
+for (int i = 0; i < sizeA; i++)
+{
+    Console.WriteLine($"Введите значение элемента массива A[{i}]: ");
+    sourceArrayA[i] = Console.ReadLine();
+    if (sourceArrayA[i].Length <= 3)
+    {
+        finalArrayF[shortElementCounter] = sourceArrayA[i];
+        shortElementCounter++;
+    }
+
+}
+
+Console.WriteLine($"Исходный массив: [{String.Join("; ", sourceArrayA)}]");
+
+Console.WriteLine($"Итоговый массив: [{String.Join("; ", finalArrayF)}]");
